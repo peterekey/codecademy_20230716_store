ed99a8b1a126eadd83119f096c5c1eb7f3b73914
Peter Key <63628694+peterekey@users.noreply.github.com>
2023-07-16 15:52:44 +1000

    Steps 4-6: Create Redux store from slice reducers

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,14 @@
 // Import createStore and combineReducers here.
+import { createStore, combineReducers } from 'redux'
 
 // Import the slice reducers here.
+import { inventoryReducer } from '../features/inventory/inventorySlice'
+import { cartReducer } from '../features/cart/cartSlice'
+import { currencyFilterReducer } from '../features/currencyFilter/currencyFilterSlice'
 
 // Create and export the store here.
+export const store = createStore(combineReducers({
+    inventory: inventoryReducer,
+    cart: cartReducer,
+    currencyFilter: currencyFilterReducer
+}))

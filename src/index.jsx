00d04533f76628067782c9201e3fb0696087a7f9
Peter Key <63628694+peterekey@users.noreply.github.com>
2023-07-16 16:16:59 +1000

    Steps 7-9: Connect Redux to the top-level React component

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './app/App'
 import './index.css'
+import { store } from './app/store'
 
 const render = () => {
   ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-      <App />
+      <App 
+        state={store.getState()}
+        dispatch={store.dispatch}
+      />
     </React.StrictMode>,
   )
   }
@@ -14,3 +18,4 @@ const render = () => {
   render()
 
   // Subscribe render to the store.
+  store.subscribe(render)
